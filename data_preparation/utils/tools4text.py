2342141e1e829ba6e63fcc0eb844256e7f02c003
thiziri <belkacemthiziri@gmail.com>
2018-10-09 17:24:15 +0200

    passage based training

--- data_preparation/utils/tools4text.py
+++ data_preparation/utils/tools4text.py
@@ -448,3 +448,37 @@ def chunkIt(seq, num=5):
         out.append(seq[int(last):int(last + avg)])
         last += avg
     return out
+
+
+def get_all_phrases(target_word, tar_passage, left_margin=10, right_margin=10):
+    """
+        Get all the phrases that contain the target word in a text tar_passage.
+        Workaround to save the output given by nltk Concordance function.
+        left_margin and right_margin allocate the number of words/pununciation before and after target word.
+
+        :param target_word: str
+        :param tar_passage: str
+        :param left_margin: int
+        :param right_margin: int
+        :return: list
+        """
+
+    # Create list of tokens using nltk function
+    tokens = nltk.word_tokenize(tar_passage)
+
+    # Create the text of tokens
+    text = nltk.Text(tokens)
+
+    # Collect all the index or offset position of the target word
+    c = nltk.ConcordanceIndex(text.tokens, key=lambda s: s.lower())
+
+    # Collect the range of the words that is within the target word by using text.tokens[start;end].
+    # The map function is used so that when the offset position - the target range < 0, it will be default to zero
+    concordance_txt = ([text.tokens[list(map(lambda x: x - left_margin if (x - left_margin) > 0 else 0, [offset]))[
+                                        0]:offset + right_margin if (
+                                                                                offset - left_margin) > 0 else offset + right_margin + abs(
+        offset - left_margin)]
+                        for offset in c.offsets(target_word)])
+
+    # join the sentences for each of the target phrase and return it
+    return [' '.join([x + ' ' for x in con_sub]) for con_sub in concordance_txt]
